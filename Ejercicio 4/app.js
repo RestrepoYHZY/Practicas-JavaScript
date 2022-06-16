34f342b23baf6f4348d8939b6a9f32e9a5df8e36
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-06-16 14:43:51 -0500

    Se realizo el ejercicio 4-JavaScript

---
+++ Ejercicio 4/app.js
@@ -0,0 +1,16 @@
+
+
+
+//Ejercicio 4--Determinar ¿Cuál es el numero mayor en un arreglo?
+
+let numeros=  [1,6,8,4,2,7,10,3,5];
+let mayor=0;
+
+for (let i=0; i<=numeros.length; i++){
+     if (numeros[i]>=mayor){
+          mayor = numeros[i]; 
+     }
+}
+
+console.log(`El número mayor es: ${mayor}`)
+
