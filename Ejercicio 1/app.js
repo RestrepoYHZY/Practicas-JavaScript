dba426e35e53fa943974729761a5c7066e8ba4f5
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-06-16 14:16:27 -0500

    Se realizo el ejercicio 1 -JavaScript

---
+++ Ejercicio 1/app.js
@@ -0,0 +1,14 @@
+
+
+
+//Ejercicio 1--Convertir libras a kilogramos
+//kilogramos = libra/ 2.2046
+
+let libras = 160;
+let kg = libras/2.2046;
+
+kg *= 100;
+kg = Math.round (kg);
+kg/=100;
+
+console.log(kg);
