398aac29d67dae31a6d69f19bd19d6f643e7e6c3
Yhaizley Restrepo <yhazrestrepo@gmail.com>
2022-06-16 14:29:22 -0500

    Se realizo el ejercicio 3-JavaScript

---
+++ Ejercicio 3/app.js
@@ -0,0 +1,13 @@
+
+
+
+//Ejercicio 3--Calcule el área de un triangulo rectángulo
+//Base 10 y altura 5
+
+let base = 10;
+let altura= 5;
+
+let area= (base * altura)/2;
+
+
+console.log(area + " es el area del triangulo rectangulo");
